4722fc31adc70ef7ec05c82037ed51f6da7f19d1
Metok Kogyal <79946757+marsbars123@users.noreply.github.com>
2022-03-04 17:35:19 +0000

    .

--- AColumnCypher.py
+++ AColumnCypher.py
@@ -18,14 +18,16 @@ In this exercise we will use numpy multidimensional arrays to encode a message a
 """
 1. Import numpy (as np) so that you have access to all numpy methods. Code: import numpy as np
 """
-
+import numpy as np
 
 """
 2. A message is defined below. Use np.array(<list>) to convert message into an array of characters, then print the array (note: you will have to convert message to a LIST of characters first.)
 
 """
 message = "THISMESSAGEISVERYSECRETX"
-
+messagelis = list(message)
+messagearray = np.array(messagelis)
+print(messagearray)
 
 
 """
@@ -33,13 +35,26 @@ message = "THISMESSAGEISVERYSECRETX"
 Note: You can calculate how many rows you will need, or the shape function can do that automatically if you make the number of rows -1.
 """
 
+messagearray.shape =  (-1, 3)
+print(messagearray)
+
 
 """
 4. Use <var> = np.transpose(<array>) to switch the rows and the columns. Print the new array.
 """
+transposed = np.transpose(messagearray)
+print(messagearray)
 
 """
 5. When you index the transposed array, you should get a row of the array. For example, <array>[0] should be the first row of characters.
 
 Declare a new variable that will store the encoded message, and set it to an empty string. Loop through the rows of the array. Use ''.join(<array>) to convert the array of characters to a string. Then add that string to the encoded message. Print the final result.
 """
+
+hello = ""
+for row in transposed:
+  joinedrow = "".join(row)
+  hello += joinedrow
+print(hello)
+
+
